5c32c6db47e4c20632cadbb04627dda306b20b26
Ginnio Sarabia <sarabia.seven@gmail.com>
2024-10-02 13:48:32 -0500

    Se modifico import { MysqlAdapter as Database } from './database-mysql/index.cjs';

--- base-ts-baileys-mysql/src/app.ts
+++ base-ts-baileys-mysql/src/app.ts
@@ -1,6 +1,6 @@
 import { join } from 'path'
 import { createBot, createProvider, createFlow, addKeyword, utils } from '@builderbot/bot'
-import { MysqlAdapter as Database } from 'src/database-mysql/index.cjs'
+import { MysqlAdapter as Database } from './database-mysql/index.cjs';
 import { BaileysProvider as Provider } from '@builderbot/provider-baileys'
 import dotenv from 'dotenv';
 
